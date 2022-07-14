c554a58f15a5112684813a2697019f3dbacfb2f1
ToxichQQQ <storozhenko.dev@gmail.com>
2022-07-14 18:45:36 +0300

    Added form

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TheHeader/>
+  <div class="parent">
+    <div class="div1">
+        <AddForm/>
+    </div>
+    <div class="div2">
+      <CardList :items="this.cardList"/>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from "@/components/TheHeader";
+import AddForm from "@/components/AddForm";
+import CardList from "@/components/CardList";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    CardList,
+    AddForm,
+    TheHeader,
+  },
+  data() {
+    return {
+      cardList: [{
+        id: '1',
+        name: 'Наименование товара',
+        description: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
+        img: '/images/item.png',
+        price: '10000'
+      },{
+        id: '1',
+        name: 'Наименование товара',
+        description: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
+        img: '/images/item.png',
+        price: '10000'
+      },{
+        id: '1',
+        name: 'Наименование товара',
+        description: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
+        img: '/images/item.png',
+        price: '10000'
+      }]
+    }
+  },
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Source Sans Pro',Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 32px;
 }
+.parent {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
+}
+
+.div1 { grid-area: 1 / 1 / 3 / 2; }
+.div2 { grid-area: 1 / 2 / 3 / 5; }
 </style>
---
+++ src/components/AddForm.vue
@@ -0,0 +1,80 @@
+<template>
+  <form class="form">
+    <div class="input-container">
+    <label class="input-label" for="item_name">Наименование товара</label>
+    <input class="input" id="item_name" placeholder="Введите наименование товара"/>
+    </div>
+
+    <div class="input-container">
+    <label class="input-label" for="item_description">Описание товара</label>
+    <textarea class="input textarea" id="item_description" placeholder="Введите описание товара"></textarea>
+    </div>
+
+    <div class="input-container">
+    <label class="input-label" for="item_link">Ссылка на изображение товара</label>
+    <input class="input" id="item_link" placeholder="Введите ссылку"/>
+    </div>
+
+    <div class="input-container">
+    <label class="input-label" for="item_price">Цена товара</label>
+    <input class="input" id="item_price" placeholder="Введите цену"/>
+    </div>
+
+    <div class="button-container">
+      <button class="form-button" type="submit">Добавить товар</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "AddForm"
+}
+</script>
+
+<style scoped>
+.form{
+  padding: 24px;
+  box-shadow: 0 20px 30px rgba(0, 0, 0, 0.04), 0 6px 10px rgba(0, 0, 0, 0.02);
+  border-radius: 4px;
+  background-color: #FFFEFB;
+}
+  .input-container{
+    margin-bottom: 16px;
+  }
+  .input-label{
+    margin-bottom: 4px;
+    font-size: 10px;
+    font-weight: 400;
+    display: block;
+  }
+  .input{
+    display: block;
+    width: 284px;
+    padding: 10px 0 10px 16px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    border: none;
+    border-radius: 4px;
+    font-size: 12px;
+    color: #B4B4B4;
+    font-family: 'Source Sans Pro', Avenir, Helvetica, Arial, sans-serif;
+  }
+  .input:last-child{
+    margin-bottom: 24px;
+  }
+  .textarea{
+    height: 108px;
+    resize: none;
+  }
+  .button-container{
+    display: flex;
+    justify-content: center;
+  }
+  .form-button{
+    width: 100%;
+    padding: 10px 0;
+    border-radius: 10px;
+    background-color: #EEE;
+    border: none;
+  }
+</style>
