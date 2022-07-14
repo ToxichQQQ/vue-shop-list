eadb9019d5e3e86a1bb907f6506fbea614c7d444
ToxichQQQ <storozhenko.dev@gmail.com>
2022-07-14 18:45:55 +0300

    Added CardList component

---
+++ src/components/CardList.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul class="list">
+    <ListItem v-for="item in items" :key="item.id" :item="item"/>
+  </ul>
+</template>
+
+<script>
+import ListItem from "@/components/ListItem";
+
+export default {
+  name: "CardList",
+  components: {ListItem},
+  props:{
+    items:Array
+  }
+}
+</script>
+
+<style scoped>
+.list{
+  margin: 0;
+  padding-left: 16px;
+  list-style-type: none;
+
+  display: flex;
+}
+</style>
